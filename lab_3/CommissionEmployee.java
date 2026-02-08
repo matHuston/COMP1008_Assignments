f38e344badeb223d3e04fd961a91800f3dcd4c8c
matHuston <thecaranar@gmail.com>
2026-02-08 17:50:28 -0500

    super() constructor added

--- lab_3/CommissionEmployee.java
+++ lab_3/CommissionEmployee.java
@@ -5,8 +5,16 @@ public class CommissionEmployee extends Employee {
     private double commissionRate;
  
     // TODO 5: Create constructor using super()
- 
+    public CommissionEmployee(String first, String last, String ssn, double sales, double rate){
+        // call superclass constructor from Employee.java
+        super(first, last, ssn); 
+        setGrossSales(sales);
+        setCommissionRate(rate);
+    }
+
     // TODO 6: Create getters and setters with validation
+    //setters with validation
+    
  
     // TODO 7: Override earnings()
  
